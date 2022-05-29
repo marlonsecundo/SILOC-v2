0b1662078125b7bffd68ec85eb6b847b75d4ebcd
davillawitte <30900829+davillawitte@users.noreply.github.com>
2022-05-29 16:23:44 -0300

    Separado js+css+html / bottom sheet

---
+++ script.js
@@ -0,0 +1,25 @@
+function showBottomSheet(){
+    let container = document.querySelector('#bottomSheetContainer');
+    let bottomSheet = document.querySelector('#bottomSheetContainer #bottomSheet');
+    
+    container.classList.add('active');
+    setTimeout(()=>{
+      bottomSheet.classList.add('active');  
+    },1)
+}
+
+function hideBottomSheet(){
+    let container = document.querySelector('#bottomSheetContainer');
+    let bottomSheet = document.querySelector('#bottomSheetContainer #bottomSheet');
+
+    bottomSheet.classList.remove('active');
+
+    setTimeout(()=>{
+        container.classList.remove('active');
+    }, 400);
+}
+
+function addCadaver(x,y){
+    var indice = x;
+    var target = y;
+}
